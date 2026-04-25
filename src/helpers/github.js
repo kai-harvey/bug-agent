b3d7c87250d77a810fafee5237fecc13b416dc65
kai-harvey <kai@kkh.io>
2026-04-25 15:44:35 -0400

    setup

---
+++ src/helpers/github.js
@@ -0,0 +1,15 @@
+class GitService {
+
+    static getRepo(url){
+        // Clone repo to a local temp address
+    }
+
+    static deleteLocalFiles(){
+        // Delete local files after processing
+    }
+
+    static addIssue(){
+        // Add issue to the repo
+    }
+
+}
